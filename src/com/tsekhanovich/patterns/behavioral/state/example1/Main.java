00923473042c987ec3ebdb71c027f9fd7d26b9bc
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2018-08-25 22:55:12 +0300

    Added pattern: state.

---
+++ src/com/tsekhanovich/patterns/behavioral/state/example1/Main.java
@@ -0,0 +1,23 @@
+package com.tsekhanovich.patterns.behavioral.state.example1;
+
+/**
+ * @author Pavel Tsekhamovich 25.08.2018
+ * <p>
+ * State
+ * Allows an object to change its behavior depending on its state.
+ * <p>
+ * + Eliminates many large conditional operators.
+ * + Concentrates in one place the code associated with a certain state.
+ * + Simplifies the context code.
+ * <p>
+ * - Can unreasonably complicate the code if there are few states and they rarely change.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Player player = new Player();
+        UI ui = new UI(player);
+        ui.init();
+    }
+}
