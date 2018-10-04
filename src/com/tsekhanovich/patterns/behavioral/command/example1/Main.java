bb4bbfd70fd5a8269c12d526b09442b2e20cace7
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2018-10-04 22:43:08 +0300

    minor fix.

--- src/com/tsekhanovich/patterns/behavioral/command/example1/Main.java
+++ src/com/tsekhanovich/patterns/behavioral/command/example1/Main.java
@@ -2,18 +2,18 @@ package com.tsekhanovich.patterns.behavioral.command.example1;
 
 /**
  * @author Pavel Tsekhanovich 22.08.2018
- * <p>
+ *
  * Command
  * Transforms an operation into an object. This allows you to transfer operations as arguments
  * when calling methods, put operations in the queue, log them,
  * and also support the cancellation of operations.
- * <p>
+ *
  * + Removes the relationship between different layers of the program.
  * + Simple undo and redo commands.
  * + Delayed start of commands.
  * + Combination of commands.
  * + Observes the principle of openness / closure.
- * <p>
+ *
  * - Complicates the code of the program at the expense of additional classes.
  */
 
