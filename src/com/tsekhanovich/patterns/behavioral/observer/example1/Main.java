3bbeb8816bd7560579e02c3b14df2278e8f05ba7
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2018-10-07 22:30:07 +0300

    minor fix.

--- src/com/tsekhanovich/patterns/behavioral/observer/example1/Main.java
+++ src/com/tsekhanovich/patterns/behavioral/observer/example1/Main.java
@@ -2,13 +2,13 @@ package com.tsekhanovich.patterns.behavioral.observer.example1;
 
 /**
  * @author Pavel Tsekhanovich 24.08.2018
- * <p>
+ *
  * Observer
- * <p>
+ *
  * + Publisher does not depend on specific subscriber classes.
  * + You can sign and unsubscribe recipients on the fly
  * realizes the principle of openness / closure.
- * <p>
+ *
  * - Observers are notified in a random order.
  * - The publisher's subscription code can only be inherited, so it's hard to
  * integrate into an existing class tree.
